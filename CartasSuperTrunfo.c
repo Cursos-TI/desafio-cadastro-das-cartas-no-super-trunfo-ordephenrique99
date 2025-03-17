0de71b91ad0f1c53f47d1f493cd252b968070f6d
ordephenrique99 <henriquepedro9999@gmail.com>
2025-03-17 01:45:43 +0000

    AJUSTES FINAIS PARA VERSÃO FINAL

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -35,10 +35,6 @@ int main(){
     printf("Quantidade de Pontos Turisticos: ");    //Solicitando o dado.
     scanf("%d", &pTur1);                            //Recebendo do usuário e armazenando o dado na váriavel.
 
-    //Inicio da operação para armazenar os dados referentes a Densidade Populacional e PIB per Capita da carta 01.
-    densPopul1 = (float) populacao1/area1;          //Operação Densidade.
-    PIBperCapita1 = (float) PIB1/populacao1;        //Operação PIB per Capita.
-
     //inicio da solicitação dos dados referente a 2ª carta.
     printf("\nInforme os dados da 2ª carta a seguir\n");
     printf("Digite o estado: ");            //Solicitando o dado.
@@ -62,34 +58,40 @@ int main(){
     printf("Quantidade de Pontos Turisticos: ");    //Solicitando o dado.
     scanf("%d", &pTur2);                            //Recebendo do usuário e armazenando o dado na váriavel.
 
-    //Inicio da operação para armazenar os dados referentes a Densidade Populacional e PIB per Capita da carta 02.
-    densPopul2 = (float) populacao2/area2;          //Operação Densidade.
-    PIBperCapita2 = (float) PIB2/populacao2;        //Operação PIB per Capita.
-
     //Inicio da exibição do dados das cartas
     //Inicio da exibição do dados da 1ª carta.
     printf("\nCarta 1:\n");                                 //Mensagem de inicio
     printf("Estado: %s\n", estado1);                        //exibindo a letra do estado 
     printf("Código: %s%s\n", estado1, id1);                 //exibindo o código da carta formado pela letra do estado + código da cidade.
     printf("Nome da Cidade: %s\n", nomeCid1);               //exibindo o nome da cidade.
-    printf("Populção: %d\n", populacao1);                   //exibindo a quantidade da população.
+    printf("População: %d\n", populacao1);                   //exibindo a quantidade da população.
     printf("Área: %.2f km²\n", area1);                      //exibindo o tamanho em área da cidade.
     printf("PIB: %.2f bilhões de reais\n", PIB1);           //exibindo o PIB da cidade.
     printf("Número de Pontos Turísticos: %d\n", pTur1);     //exibindo a quantidade de pontos turisticos da cidade.
-    printf("Densidade Populacional: %.2f hab/km²", densPopul1); //exibindo a densidade populacional.
-    printf("PIB per Capita: %.2f reais", PIBperCapita1);        //exibindo o PIB per Capita.
+    
+    //Inicio da operação para armazenar os dados referentes a Densidade Populacional e PIB per Capita da carta 01.
+    densPopul1 = (float) populacao1/area1;          //Operação Densidade.
+    PIB1 = PIB1*1000000000;
+    PIBperCapita1 = (float) PIB1/populacao1;        //Operação PIB per Capita.
+    printf("Densidade Populacional: %.2f hab/km²\n", densPopul1); //exibindo a densidade populacional.
+    printf("PIB per Capita: %.2f reais\n", PIBperCapita1);        //exibindo o PIB per Capita.
 
     //Inicio da exibição do dados da 2ª carta.
     printf("\nCarta 2:\n");                                 //Mensagem de inicio
     printf("Estado: %s\n", estado2);                        //exibindo a letra do estado 
     printf("Código: %s%s\n", estado2, id2);                 //exibindo o código da carta formado pela letra do estado + código da cidade.
     printf("Nome da Cidade: %s\n", nomeCid2);               //exibindo o nome da cidade.
-    printf("Populção: %d\n", populacao2);                   //exibindo a quantidade da população.
+    printf("População: %d\n", populacao2);                   //exibindo a quantidade da população.
     printf("Área: %.2f km²\n", area2);                      //exibindo o tamanho em área da cidade.
     printf("PIB: %.2f bilhões de reais\n", PIB2);           //exibindo o PIB da cidade.
     printf("Número de Pontos Turísticos: %d\n", pTur2);     //exibindo a quantidade de pontos turisticos da cidade.
-    printf("Densidade Populacional: %.2f hab/km²", densPopul2); //exibindo a densidade populacional.
-    printf("PIB per Capita: %.2f reais", PIBperCapita2);        //exibindo o PIB per Capita.
+
+    //Inicio da operação para armazenar os dados referentes a Densidade Populacional e PIB per Capita da carta 02.
+    densPopul2 = (float) populacao2/area2;          //Operação Densidade.
+    PIB2 = PIB2*1000000000;
+    PIBperCapita2 = (float) PIB2/populacao2;        //Operação PIB per Capita.
+    printf("Densidade Populacional: %.2f hab/km²\n", densPopul2); //exibindo a densidade populacional.
+    printf("PIB per Capita: %.2f reais\n", PIBperCapita2);        //exibindo o PIB per Capita.
 
     return 0;
 }
