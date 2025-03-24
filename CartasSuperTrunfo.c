fc9465ddeb6662694c6ee03e7cb02d3a4584b1f3
ordephenrique99 <henriquepedro9999@gmail.com>
2025-03-24 23:44:37 +0000

    VERSÃO FINAL - NOVATO 2.0

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -113,10 +113,10 @@ int main(){
     printf("\nCARTA 01 %s - POPULAÇÃO: %lu", nomeCid1, populacao1);
     printf("\nCARTA 01 %s - POPULAÇÃO: %lu\n", nomeCid2, populacao2);
     if(populacao1 > populacao2){                        //Verificação se valor da Carta 1 é maior que a Carta 2
-        printf("Carta 1 %s venceu!", nomeCid1);         //Caso carta 1 for maior valor apresenta mensagem de vencedor
+        printf("Carta 1 %s venceu!\n", nomeCid1);       //Caso carta 1 for maior valor apresenta mensagem de vencedor
     }
     else{                                                           
-        printf("Carta 2 %s venceu!", nomeCid2);         //Caso carta 2 for maior valor apresenta mensagem de vencedor
+        printf("Carta 2 %s venceu!\n", nomeCid2);       //Caso carta 2 for maior valor apresenta mensagem de vencedor
     }
   
     return 0;
